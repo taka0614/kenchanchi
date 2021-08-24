3e853870576199492a4f2e673834bebcd5cdb399
taka0614 <88304537+taka0614@users.noreply.github.com>
2021-08-24 18:08:23 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -14,12 +14,29 @@ $(function() {
 $(function() {
   $('.hambmenu_anchor').click(function() {
       $('.globalMenuSp').removeClass('active');
+      $('.hamburger').removeClass('active');
   });
 });
 
 $(function() {
-  $('.hambmenu_anchor').click(function() {
+  $('.page_top a').click(function() {
       $('.hamburger').removeClass('active');
+      $('.globalMenuSp').removeClass('active');
   });
+});
+
+$(function(){
+  var pagetop = jQuery('#page_top');
+  // ボタン非表示
+  pagetop.hide();
+  // 100px スクロールしたらボタン表示
+  $(window).scroll(function () {
+     if ($(this).scrollTop() > 1500) {
+          pagetop.fadeIn();
+     } else {
+          pagetop.fadeOut();
+     }
+  });
+  
 });
 
